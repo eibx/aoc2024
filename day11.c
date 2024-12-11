a046b01c27c9b8fe5394db28f5d5fd4395694d8b
Eibx <jesper@eibx.com>
2024-12-11 20:58:59 +0100

    test

---
+++ day11.c
@@ -0,0 +1,88 @@
+#include "basic.c"
+#include <math.h>
+
+u64 num_digits(u64 n) {
+    return
+(n > 999999999999999999) ? 19 :
+(n > 99999999999999999) ? 18 :
+(n > 9999999999999999) ? 17 :
+(n > 999999999999999) ? 16 :
+(n > 99999999999999) ? 15 :
+(n > 9999999999999) ? 14 :
+(n > 999999999999) ? 13 :
+(n > 99999999999) ? 12 :
+(n > 9999999999) ? 11 :
+(n > 999999999) ? 10 :
+(n > 99999999) ? 9 :
+(n > 9999999) ? 8 :
+(n > 999999) ? 7 :
+(n > 99999) ? 6 :
+(n > 9999) ? 5 :
+(n > 999) ? 4 :
+(n > 99) ? 3 :
+(n > 9) ? 2 : 1;
+}
+
+u64 part_1(u8* content, u32 file_size) {
+    u64* stones = (u64*)malloc(50000000*sizeof(u64));
+    u64 stone_count = 0;
+
+    u64* stones_new = (u64*)malloc(50000000*sizeof(u64));
+    u64 stone_new_count = 0;
+
+    u64 n = 0;
+    for (i32 i = 0; i < file_size; i++) {
+        if (content[i] >= '0' && content[i] <= '9') {
+            n *= 10;
+            n += content[i]-'0';
+        } else {
+            stones[stone_count++] = n;
+            n = 0;
+        }
+    }
+
+    for (u64 i = 0; i < 75; i++) {
+        for (u64 j = 0; j < stone_count; j++) {
+            u64 number = stones[j];
+            if (number == 0) {
+                stones_new[stone_new_count++] = 1;
+                continue;
+            }
+
+            u64 digits = num_digits(number);
+            if (digits % 2 == 0) {
+                u64 power = (u64)pow(10, (i32)(digits / 2));
+                u64 left = (u64)(number / power);
+                u64 right = number - (left * power);
+                for (u64 k = stone_count; k > j; k--) {
+                    stones_new[k] = stones[k-1];
+                }
+                stones_new[stone_new_count++] = left;
+                stones_new[stone_new_count++] = right;
+            } else {
+                stones[stone_new_count++] = number * 2024;
+            }
+        }
+
+        u64* tmp = stones;
+        stones = stones_new;
+        stones_new = tmp;
+        stone_count = stone_new_count;
+        stone_new_count = 0;
+    }
+
+    return stone_count;
+}
+
+u64 part_2(u8* content, u32 file_size) {
+    return 0;
+}
+
+i32 main() {
+    u32 file_size;
+    u8* content = load_file("input11.txt", &file_size);
+    printf("part 1: %llu\n", part_1(content, file_size));
+    printf("part 2: %llu\n", part_2(content, file_size));
+
+    return 0;
+}
